ff3e760da77ab7ceeaeb0307d5f99f9c6ce540ae
mlondong <mauricio.londono@gmail.com>
2021-08-18 15:27:18 -0500

    added class Message and change MessageDTO

--- src/main/java/co/com/foodbank/message/dto/MessageDTO.java
+++ src/main/java/co/com/foodbank/message/dto/MessageDTO.java
@@ -1,7 +1,6 @@
 package co.com.foodbank.message.dto;
 
 import java.util.Date;
-import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -34,7 +33,6 @@ public class MessageDTO {
     private String description;
 
     @NotNull
-    @Valid
     private UserData user;
 
 }
--- src/main/java/co/com/foodbank/message/dto/interfaces/IMessage.java
+++ src/main/java/co/com/foodbank/message/dto/interfaces/IMessage.java
@@ -1,11 +1,18 @@
 package co.com.foodbank.message.dto.interfaces;
 
 import java.util.Date;
-import co.com.foodbank.user.dto.request.RequestUserData;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import co.com.foodbank.message.dto.Message;
+import co.com.foodbank.user.dto.response.UserData;
 
 /**
  * @author mauricio.londono@gmail.com co.com.foodbank.message.dto 9/08/2021
  */
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY,
+        property = "type")
+@JsonSubTypes({@Type(value = Message.class, name = "Message")})
 public interface IMessage {
 
     String getId();
@@ -16,5 +23,5 @@ public interface IMessage {
 
     String getDescription();
 
-    RequestUserData getUser();
+    UserData getUser();
 }
